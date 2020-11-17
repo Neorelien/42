ac2390e61359486016e0638044696a0d4fc70022
Neorelien <aurelienwery08@gmail.com>
2020-11-17 23:08:03 +0100

    memchr

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: awery <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 13:59:03 by awery             #+#    #+#             */
-/*   Updated: 2020/11/17 22:31:47 by awery            ###   ########.fr       */
+/*   Updated: 2020/11/17 23:07:33 by awery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memchr(void *dst, int c, size_t n);
 
 int main() {
 
     int array [] = { 54, 85, 20, 63, 21 };
-    int src[] = {1,2, 7};
-    size_t n = sizeof( int ) * 3;
+    size_t n = sizeof( int ) * 2;
 
     /* Display the initial values */
     for(int  length=0; length<5; length++) {
@@ -29,7 +28,7 @@ int main() {
     printf( "\n" );
 
     /* Reset the memory bloc */
-    printf("%p\n", ft_memmove( array, src, n ));
+    printf("%p\n", ft_memchr( array, 54, n ));
 
     /* Display the new values */
     for( int length=0; length<5; length++) {
@@ -38,7 +37,7 @@ int main() {
     printf( "\n" );
 
      /* Reset the memory bloc */
-    printf("%p\n", memmove( array, src,  n ));
+    printf("%p\n", memchr( array, 54, n ));
 
     /* Display the new values */
     for( int length=0; length<5; length++) {
