675a069363a89720fa867ecf5582cf6fae89f484
Aurelien WERY <aurelienwery08@gmail.com>
2021-01-13 18:17:41 +0100

    ok

--- Aurel/Cube3D/Cube3D.c
+++ Aurel/Cube3D/Cube3D.c
@@ -6,21 +6,52 @@
 /*   By: awery <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 11:12:11 by awery             #+#    #+#             */
-/*   Updated: 2021/01/13 11:49:05 by awery            ###   ########.fr       */
+/*   Updated: 2021/01/13 16:25:55 by awery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilibx/mlx.h"
 #include <stdio.h>
 
+typedef struct s_lolol
+{
+	void	*mlx_ptr;
+	void	*windows_identifier;
+	int		a;
+}				t_lolol;
+
+int	mouuve(int ascii, t_lolol *param)
+{
+	printf("\n%d\n", ascii);
+	int i = 0;
+	if (ascii == 0)
+	{
+		param->a = param->a + 20;
+		mlx_clear_window (param->mlx_ptr, param->windows_identifier);
+		while (i++ < 230)
+			mlx_pixel_put(param->mlx_ptr, param->windows_identifier, param->a, i, 0255076);
+	}
+	return (0);
+}
+int	exposetest(t_lolol *param)
+{
+	param->a++;
+	printf("ouiouio");
+	return (0);
+}
+	
 int main()
 {
-	void	*mlx;
-	void	*windows_identifier;
+	t_lolol huli;
+	int		i = 0;
+	huli.a = 0;
 
-	mlx = mlx_init();
-	
-	windows_identifier =  mlx_new_window(mlx, 500, 500, "bonjour");
-	mlx_pixel_put(mlx, windows_identifier, 0, 0, 1000);
-	mlx_loop (mlx);
+	huli.mlx_ptr = mlx_init();
+	huli.windows_identifier =  mlx_new_window(huli.mlx_ptr, 500, 500, "bonjour");
+	while (i++ < 230)
+		mlx_pixel_put(huli.mlx_ptr,huli. windows_identifier, huli.a, i, 0255076);
+	mlx_string_put(huli.mlx_ptr, huli.windows_identifier, 255, 255, 0255076, "YOO LA MIF" );
+	mlx_key_hook(huli.windows_identifier, &mouuve, &huli);
+	mlx_expose_hook(huli.windows_identifier, &exposetest, &huli);
+	mlx_loop (huli.mlx_ptr);
 }
