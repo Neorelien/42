978166c2dfeb9131b8f724bf293b24d0bd380da4
Neorelien <aurelienwery08@gmail.com>
2021-01-14 19:15:00 +0100

    autocommit

--- Aurel/Cube3D/Cube3D.c
+++ Aurel/Cube3D/Cube3D.c
@@ -6,11 +6,23 @@
 /*   By: Aurelien <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 18:14:40 by Aurelien          #+#    #+#             */
-/*   Updated: 2021/01/14 19:08:04 by Aurelien         ###   ########.fr       */
+/*   Updated: 2021/01/14 19:13:00 by Aurelien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cube3D_utils.h"
+
+ft_map_dup(char **map)
+{
+	int x;
+	int y;
+
+	y = 0;
+	x = 0;
+	while (map[x] != '\0')
+		x++;
+
+}
 
 int	ft_check_map(char **map)
 {
@@ -31,7 +43,7 @@ int	ft_map_init(int argc, char **argv, t_data *mlx)
 	nxt_line = get_next_line(fd, mlx->map);
 	while (nxt_line != -1)
 	{
-		
+		temp = ft_map_dup(mlx->map);
 	}
 	if (ft_check_map(mlx->map))
 		return (1);
