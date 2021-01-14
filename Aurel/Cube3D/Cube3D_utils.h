da28decd778aa238e9ffeea0d5fb22f0f40e44d7
Neorelien <aurelienwery08@gmail.com>
2021-01-14 23:00:01 +0100

    autocommit

--- Aurel/Cube3D/Cube3D_utils.h
+++ Aurel/Cube3D/Cube3D_utils.h
@@ -6,17 +6,16 @@
 /*   By: aurelien <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 11:10:07 by aurelien          #+#    #+#             */
-/*   Updated: 2021/01/14 22:53:50 by Aurelien         ###   ########.fr       */
+/*   Updated: 2021/01/14 22:55:45 by Aurelien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilibx/mlx.h"
 #include "libft.h"
 
-char **map;
-
 typedef struct	s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	char	**map;
 }				t_data;
