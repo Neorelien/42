dc94a27f1074683afac9017281ca71c65bcba519
Neorelien <aurelienwery08@gmail.com>
2021-01-14 18:25:00 +0100

    autocommit

--- Aurel/Cube3D/Cube3D.c
+++ Aurel/Cube3D/Cube3D.c
@@ -6,10 +6,21 @@
 /*   By: Aurelien <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 18:14:40 by Aurelien          #+#    #+#             */
-/*   Updated: 2021/01/14 18:15:03 by Aurelien         ###   ########.fr       */
+/*   Updated: 2021/01/14 18:22:43 by Aurelien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cube3D_utils.h"
 
+int	ft_map_init(char *file)
+{
+	open
+	
+}
 
+int main(int argc, char *argv)
+{
+	if (ft_map_init(argc))
+		return (1);
+	return (0);
+}
