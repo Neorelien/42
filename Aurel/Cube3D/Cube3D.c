701687a808de184d6726c881a09471335995be07
Neorelien <aurelienwery08@gmail.com>
2021-01-15 18:00:00 +0100

    autocommit

--- Aurel/Cube3D/Cube3D.c
+++ Aurel/Cube3D/Cube3D.c
@@ -6,7 +6,7 @@
 /*   By: Aurelien <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 18:14:40 by Aurelien          #+#    #+#             */
-/*   Updated: 2021/01/15 17:54:36 by Aurelien         ###   ########.fr       */
+/*   Updated: 2021/01/15 17:56:30 by Aurelien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,7 +128,10 @@ int			ft_get_elements(char **argv, t_data *mlx)
 		return (1);
 	if (get_next_line(fd, line) == -1)
 		return (1);;
-	
+	while ((element = ft_map_element(line) != 0))
+			{
+
+			}
 	return (0);	
 }
 
