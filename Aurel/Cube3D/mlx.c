1ac3744b53b4e89be9d9512f32a2b7aaae190651
Neorelien <aurelienwery08@gmail.com>
2021-01-17 22:45:00 +0100

    autocommit

--- Aurel/Cube3D/mlx.c
+++ Aurel/Cube3D/mlx.c
@@ -6,7 +6,7 @@
 /*   By: Aurelien <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 21:44:16 by Aurelien          #+#    #+#             */
-/*   Updated: 2021/01/17 21:53:46 by Aurelien         ###   ########.fr       */
+/*   Updated: 2021/01/17 22:43:09 by Aurelien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ int	ft_init_mlx(t_data *mlx)
 	mlx->mlx_ptr = mlx_init();
 	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr,
 			mlx->element.x, mlx->element.y, "Cube3D");
-	mlx_loop(mlx->mlx_ptr);
 	return (0);
 }
