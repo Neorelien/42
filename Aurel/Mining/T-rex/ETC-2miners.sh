306f057e5ba9c9ca164996f351520a86aeb27830
Aurelien <aurelienwery08@gmail.com>
2021-03-02 19:25:52 +0100

    ajout mining

---
+++ Aurel/Mining/T-rex/ETC-2miners.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex -a etchash -o stratum+tcp://etc.2miners.com:1010 -u 0xa99a027396D6f171695aa0a43978850658fc60DF -p x -w rig0
---
+++ Aurel/Mining/T-rex/ethmine.sh
@@ -0,0 +1,2 @@
+#!/bin/sh                                                                       
+./t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0xa99a027396D6f171695aa0a43978850658fc60DF -p x -w rig0
