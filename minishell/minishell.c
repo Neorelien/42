3cb4064e81d9ce52ade8a39ede950824a3f4c7d3
Aurelien WERY <aurelienwery08@gmail.com>
2021-03-03 11:50:30 +0100

    aurel 1

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -6,8 +6,29 @@
 /*   By: awery <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 11:25:08 by awery             #+#    #+#             */
-/*   Updated: 2021/03/03 11:25:32 by awery            ###   ########.fr       */
+/*   Updated: 2021/03/03 11:49:52 by awery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell_utils.h"
+
 int main (int argc, char **argv)
+{
+	char		**line;
+	t_parsing	*parsing;
+	int			i;
+	char		**splited;
+	
+	i = 0;
+	line = malloc(sizeof(char*) * 1);
+	*line = NULL;
+	splited = NULL;
+	while (splited != NULL ||  get_next_line(1, line))
+	{
+		splited = ft_split(*line, ' ');
+		
+		free(*line);
+		*line = NULL;
+	}
+	return (0);
+}
