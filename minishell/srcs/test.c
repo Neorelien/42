d144c4c4a30627e2ac5eaf73e857357728116c0b
lol <lol@gmail.com>
2021-03-04 20:43:18 +0100

    lol

--- minishell/srcs/test.c
+++ minishell/srcs/test.c
@@ -1,11 +1,24 @@
 #include "../minishell_utils.h"
 #include "libft/libft.h"
-int main(int argc, char **argv)
+char **g_envv;
+
+int main(int argc, char **argv, char **envv)
 {
 	char *lol;
-	lol = NULL;
-	chdir("../");
-	printf("%s\n", getcwd(lol, 0 ));
+int i, j;
+i = 0;
+j = 0;
+	while (envv[i])
+{
+	if (ft_strncmp(envv[i], "HOME=", 4) == 0)
+	{
+		while (envv[i][j] != '=')
+			j++;
+		lol = envv[i] + j + 1;
+		printf("%s\n", lol);
+	}
+	i++;
+}
 
 /*	t_parsing info;
 
