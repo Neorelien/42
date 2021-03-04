0d99b572ccea67928b153b0ada95d36099c15a2b
Clement MOYAL <cmoyal@e1r5p14.42.fr>
2021-03-04 12:49:59 +0100

    clem

--- minishell/srcs/echo.c
+++ minishell/srcs/echo.c
@@ -6,7 +6,7 @@
 /*   By: cmoyal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:16:17 by cmoyal            #+#    #+#             */
-/*   Updated: 2021/03/04 12:08:25 by cmoyal           ###   ########.fr       */
+/*   Updated: 2021/03/04 12:49:46 by cmoyal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../minishell_utils.h"
@@ -36,8 +36,6 @@ int		echo(t_parsing info)
 	int		fd;
 
 	i = 0;
-	if (ft_doubletab_len(info.option) > 1)
-		return (0);
 	if (ft_strncmp(info.data[i], "-n", 3) == 0 && ++i)
 		flag = 1;
 	if ((fd = echo_separator(info)) <= 0)
