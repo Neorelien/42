6751ffc8099f75f9828eada62e0502c7aa6b347b
Aurelien WERY <aurelienwery08@gmail.com>
2021-03-09 13:03:52 +0100

    ok

--- minishell/srcs/other_executables.c
+++ minishell/srcs/other_executables.c
@@ -6,7 +6,7 @@
 /*   By: awery <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 13:55:18 by awery             #+#    #+#             */
-/*   Updated: 2021/03/09 12:06:43 by awery            ###   ########.fr       */
+/*   Updated: 2021/03/09 13:03:27 by awery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,8 @@ void	ft_other_exc(t_parsing *parsing, char **env, t_utils *utils)
 {
 	(void)env;
 	(void)parsing;
-	if (pipe(pipefd) == -1)
-	{
-perror
-	}
-	fork();
+	if (pipe(utils->pipefd) == -1)
+	;
+	//fork();
 
 }
