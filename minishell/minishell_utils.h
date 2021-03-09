7a16cd051ea7e3e48e3e8e9a809eeb090f83d6a4
Aurelien WERY <aurelienwery08@gmail.com>
2021-03-09 13:36:07 +0100

    ajout fork

--- minishell/minishell_utils.h
+++ minishell/minishell_utils.h
@@ -6,7 +6,7 @@
 /*   By: awery <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 11:29:02 by awery             #+#    #+#             */
-/*   Updated: 2021/03/09 13:22:35 by awery            ###   ########.fr       */
+/*   Updated: 2021/03/09 13:32:30 by awery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,19 +43,20 @@ typedef struct	s_utils
 # define OPEN_SQUOTE -2
 # define OPEN_DQUOTE -3
 
-void	ft_other_exc(t_parsing *parsing, char **env, t_utils *utils);
-int		ft_unset(t_parsing *parsing, char ***env);
-int		ft_env(t_parsing *parsing, char **env);
-int		ft_error(char *str, char *strbis);
-char	**recopy_data(char **data, char **temp);
-int		ft_export(t_parsing *parsing, char ***env);
-char	*ft_str_erase_set(char *str, char quote);
-int		echo(t_parsing info, char ***env);
-size_t	ft_doubletab_len(char **doubletab);
-int		is_separator(char *str);
-int		write_with_separator(t_parsing info);
-int		ft_display_rep(char **env, t_utils utils);
-int		ft_cd(t_parsing info, char ***env, t_utils *utils);
-int 	ft_pwd(t_parsing info, char ***env, t_utils utils);
-int		env_in_env(char **env, char *str);
+t_parsing	*new_list(t_parsing *previous_lst);
+void		ft_other_exc(t_parsing *parsing, char **env, t_utils *utils);
+int			ft_unset(t_parsing *parsing, char ***env);
+int			ft_env(t_parsing *parsing, char **env);
+int			ft_error(char *str, char *strbis);
+char		**recopy_data(char **data, char **temp);
+int			ft_export(t_parsing *parsing, char ***env);
+char		*ft_str_erase_set(char *str, char quote);
+int			echo(t_parsing info, char ***env);
+size_t		ft_doubletab_len(char **doubletab);
+int			is_separator(char *str);
+int			write_with_separator(t_parsing info);
+int				ft_display_rep(char **env, t_utils utils);
+int			ft_cd(t_parsing info, char ***env, t_utils *utils);
+int			ft_pwd(t_parsing info, char ***env, t_utils utils);
+int			env_in_env(char **env, char *str);
 #endif
