596b090bffba52f2d7df54a731ac7e0850fe451d
Clement MOYAL <cmoyal@e1r9p12.42.fr>
2021-03-23 16:28:11 +0100

    clem

--- minishell/srcs/echo.c
+++ minishell/srcs/echo.c
@@ -6,7 +6,7 @@
 /*   By: cmoyal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:16:17 by cmoyal            #+#    #+#             */
-/*   Updated: 2021/03/23 16:11:52 by cmoyal           ###   ########.fr       */
+/*   Updated: 2021/03/23 16:27:52 by cmoyal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int		ft_echo(t_parsing info, char ***env, t_utils *utils)
 	int		flag;
 	int		fd;
 	char	*str;
+	int		flag_pipe;
 
+	flag_pipe = ft_next_is_pipe(info, *env, utils, 0);
 	flag = 0;
 	i = 0;
 	fd = write_with_separator(info, *env, utils, 1);
@@ -42,13 +44,14 @@ int		ft_echo(t_parsing info, char ***env, t_utils *utils)
 	}
 	if (flag == 0)
 		ft_putstr_fd("\n", fd);
-	if (g_sig.pid > 0)
+
+/*	if (g_sig.pid > 0)
 	{
 		wait(NULL);
 		g_sig.pid = -1;
 		close(fd);
 		return (0);
-	}
+	}*/
 //	if (fd != 1)
 //		close(fd);
 	return (1);
