ce08bbc5d6e9f1b18b868b36565970e63d048a9c
lol <lol@gmail.com>
2021-04-09 14:54:22 +0200

    clem

--- minishell/srcs/echo.c
+++ minishell/srcs/echo.c
@@ -6,36 +6,45 @@
 /*   By: cmoyal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:16:17 by cmoyal            #+#    #+#             */
-/*   Updated: 2021/04/07 15:12:10 by cmoyal           ###   ########.fr       */
+/*   Updated: 2021/04/09 14:39:57 by cmoyal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell_utils.h"
 
+int		ft_echo_flag(t_parsing info, int *i)
+{
+	int flag;
+	int o;
+
+	flag = 0;
+	o = 1;
+	while (ft_strncmp(info.data[*i], "-n", 1) == 0)
+	{
+		while (info.data[*i][o] && info.data[*i][o] == 'n')
+			o++;
+		if (info.data[*i][o] == 0)
+		{
+			flag = 1;
+			i[0]++;
+		}
+		else
+			break ;
+	}
+	return (flag);
+}
+
 int		ft_echo(t_parsing info, char ***env, t_utils *utils)
 {
 	int		i;
 	int		flag;
-	int		o;
 
 	(void)env;
-	flag = 0;
 	i = 0;
-	o = 1;
+	flag = 0;
 	if (info.data)
 	{
-		while (ft_strncmp(info.data[i], "-n", 1) == 0)
-		{
-			while (info.data[i][o] && info.data[i][o] == 'n')
-				o++;
-			if (info.data[i][o] == 0)
-			{
-				flag = 1;
-				i++;
-			}
-			else
-				break ;
-		}
+		flag = ft_echo_flag(info, &i);
 		while (info.data[i])
 		{
 			ft_putstr_fd(info.data[i], 1);
